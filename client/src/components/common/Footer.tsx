4215cc6866813ef93cb2e9c598dabca26a183816
SachJaiswal <sachinjaiswal382004@gmail.com>
2025-11-13 19:57:52 +0530

    Updated box shadow

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
             <span>
-                Build with ❤️ by{" "}
+                Build with ❤️ by{"B-12"}
                 <a
                     href="https://github.com/sachJaiswal"
                     className="text-primary underline underline-offset-1"
--- client/src/components/sidebar/tooltipStyles.ts
+++ client/src/components/sidebar/tooltipStyles.ts
@@ -5,7 +5,7 @@ export const tooltipStyles = {
     color: '#000',
     fontSize: '12px',
     fontWeight: '500',
-    boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+    boxShadow: '0 4px 6px -1px rgba(0, 4, 244, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
 }
 
 export const buttonStyles = {
